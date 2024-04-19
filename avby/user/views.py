7fb7fc99b39dd8fdd20956f857ca4b6a2ca9a36d
Z1pP <72826832+Z1pP@users.noreply.github.com>
2024-04-19 05:37:46 +0300

    Update views.py
    рефакторинг

--- avby/user/views.py
+++ avby/user/views.py
@@ -1,11 +1,16 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions, status
+from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .serializers import UserSerializer, UserLoginSerializer, UserRegisterSerializer
+from .serializers import (
+    UserSerializer,
+    UserLoginSerializer,
+    UserRegisterSerializer,
+    UserUpdateSerializer,
+)
 
 
 # Create your views here.
@@ -19,14 +24,14 @@ class UserRegisterView(generics.CreateAPIView):
         if serializer.is_valid():
             user = serializer.save()
             if not user:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                return Response(serializer.errors, status=400)
             refresh = RefreshToken.for_user(user)
             response_data = {
                 'refresh': str(refresh),
                 'access': str(refresh.access_token),
             }
-            return Response(response_data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(response_data, status=201)
+        return Response(serializer.errors, status=400)
 
 
 class UserListView(generics.ListAPIView):
@@ -35,9 +40,15 @@ class UserListView(generics.ListAPIView):
     permission_classes = (AllowAny,)
 
 
-class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+class UserDetailView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class UserUpdateView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserUpdateSerializer
+    permission_classes = (AllowAny,)
+    lookup_field = "pk"
 
 
 class UserLoginView(TokenObtainPairView):
