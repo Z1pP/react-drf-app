87a38a54165ba3dbbde36c01ecd312f1708ddab3
Z1pP <72826832+Z1pP@users.noreply.github.com>
2024-05-06 18:18:25 +0300

    add sale page to routers

--- avby-front/src/components/Body/BodyLayout.jsx
+++ avby-front/src/components/Body/BodyLayout.jsx
@@ -7,6 +7,7 @@ import Logout from "../../pages/LoginPage/Logout";
 import RegisterPage from "../../pages/RegisterPage/RegisterPage";
 import Profile from "../../pages/ProfilePage/ProfilePage";
 import CarPage from "../../pages/CarPage/CarPage";
+import SalePage from "../../pages/SalePage/SalePage";
 
 export default function BodyLayout() {
   return (
@@ -16,6 +17,7 @@ export default function BodyLayout() {
       <Route path="logout" element={<Logout />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/sale" element={<SalePage />} />
       <Route path="/cars/:brand/:model/:id" element={<CarPage/>} />
     </Routes>
   );
