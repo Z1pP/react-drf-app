13c581dfff293c257c46e0cfcbfdcb52fb33d0ca
Z1pP <72826832+Z1pP@users.noreply.github.com>
2024-05-06 18:20:58 +0300

    add token verification

--- avby-front/src/App.jsx
+++ avby-front/src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -9,7 +9,7 @@ import BodyLayout from "./components/Body/BodyLayout.jsx";
 import MessageBlock from "./components/MessageBlocks/MessageBlock.jsx";
 
 import { verifyToken } from "./services/APIService.js";
-import { verifyUser } from "./Redux/reducers/authSlice.js";
+import { login, logout } from "./Redux/reducers/authSlice.js";
 import { messageInfoAction } from "./Redux/reducers/messageInfoSlice.js";
 
 import "./App.css";
@@ -18,20 +18,36 @@ export default function App() {
   const dispatch = useDispatch();
   const token = useSelector((state) => state.auth.token);
 
-  React.useEffect(async () => {
-    if (token) {
-      try {
-        const responce = await verifyToken(token);
-        dispatch(verifyUser(responce.data));
-      } catch (error) {
+  React.useEffect(() => {
+    const verifyUserToken = async () => {
+      if (token) {
+        try {
+          const response = await verifyToken(token);
+          if (response.status === 200) {
+            const refreshToken = localStorage.getItem("refreshToken");
+            dispatch(login({ accessToken: token, refreshToken: refreshToken }));
+          } else {
+            dispatch(logout());
+            return <Navigate to="/login" />;
+          }
+        } catch (error) {
+          if (error.response && error.response.status === 401) {
+            dispatch(logout());
+            return <Navigate to="/login" />;
+          }
+        }
+      } else {
         dispatch(
           messageInfoAction({
             type: "error",
-            text: "Токен устарел, пожалуйста авторизуйтесь заново",
+            text: "Токен устарел, пожалуйста авторизуйтесь заново",
           })
         );
+        return <Navigate to="/login" />;
       }
-    }
+    };
+
+    verifyUserToken();
   }, [token, dispatch]);
 
   return (
