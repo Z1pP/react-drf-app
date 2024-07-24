338c6fba77d1c433bf178175c303963a380149f6
Max Blanc <maxm_99@mail.ru>
2024-07-25 00:35:12 +0300

    Удаление файлов из индекса согласно .gitignore

--- avby/cars/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CarsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cars'
-    verbose_name = 'Автомобили'
--- avby/telegram/bot/loader.py
+++
@@ -1,7 +0,0 @@
-from telebot import TeleBot
-from telebot.storage import StateMemoryStorage
-
-TOKEN = '6302381200:AAHcCUgGf2DLQyPlqsypsDS5mmr6SapmlEs'
-
-bot = TeleBot(TOKEN, parse_mode='HTML', state_storage=StateMemoryStorage())
-
